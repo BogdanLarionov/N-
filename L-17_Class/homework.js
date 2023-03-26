ce004e1c87576c579733f75cd558997a95b28998
Bogdan Larionov <larionovbogdan1990@gmail.com>
2023-03-26 19:06:55 +0200

    homework added

---
+++ L-17_Class/homework.js
@@ -0,0 +1,83 @@
+// Дан массив с данными
+const people = [
+  {
+    first_name: "Ivan",
+    last_name: "Ivanov",
+    age: 20,
+    city: "Moscow",
+    country: "Russia",
+    active: true,
+  },
+  {
+    first_name: "dmitry",
+    last_name: "Petrov",
+    age: 7,
+    city: "Berlin",
+    country: "Germany",
+    active: true,
+  },
+  {
+    first_name: "Irina",
+    last_name: "Alexandrova",
+    age: 46,
+    city: "Paris",
+    country: "France",
+    active: false,
+  },
+  {
+    first_name: "Denis",
+    last_name: "Sokolov",
+    age: 10,
+    city: "Rome",
+    country: "Italy",
+    active: false,
+  },
+];
+
+//1. Сформируйте массив строк - каждая строка включает в себя информацию по каждому человеку в следующем формате: "<Имя> <Фамилия> (<Город>, <Страна>)". Пример => ['Ivan Ivanov (Moscow, Russia)', {...}, {...}, ...]
+
+let new_arr = people.map(
+  (elem) =>
+    `${elem.first_name} ${elem.last_name} (${elem.city}, ${elem.country})`
+);
+console.log(new_arr);
+
+// 2. Сформируйте массив из активных людей
+
+let newArr = people.filter((elem) => elem.active === true);
+let new_Arr = people.filter((elem) => elem.active); // короткий способ
+console.log(newArr); //  Сформирован массив из активных людей!
+console.log(new_Arr); //  Сформирован массив из активных людей!
+
+//3. Сформируйте массив из людей, чьи имена начинаются на 'D' (регистр буквы может быть и нижним)
+
+let arr_name_d = people.filter(
+  (elem) => elem.first_name[0] === "D" || elem.first_name[0] === "d"
+); // получили массив
+
+let arr_name_D = people.filter(
+  (elem) => elem.first_name[0].toUpperCase() === "D"
+);
+console.log(arr_name_d); // второй метод
+console.log(arr_name_D); // второй метод
+
+//4. Получите из массива объект, где страной проживания человека является 'Germany'
+
+let getCountry = people.find((elem) => elem.country === "Germany");
+console.log(getCountry); // country: 'Germany',
+
+// 5. Сформируйте массив из совершеннолетних пользователей. И выведите в консоль результат в формате: [ {<Имя> <Фамилия> (<возраст>), <Страна> (<Город>)}, {...}, {...}, ...]
+
+let getAdultPeople = people
+  .filter((elem) => elem.age >= 18)
+  .map(
+    (elem) =>
+      `${elem.first_name} ${elem.last_name} (${elem.age}), ${elem.country} (${elem.city})`
+  );
+console.log(getAdultPeople); // [ 'Ivan Ivanov (20), Russia (Moscow)','Irina Alexandrova (46), France (Paris)' ]
+
+// 6. Измените у первого элемента значение first_name на "Petr" и age на 99;
+
+people[0].first_name = "Petr"; // поменяли имя
+people[0].age = 99; // поменяли возрост
+console.log(people);
