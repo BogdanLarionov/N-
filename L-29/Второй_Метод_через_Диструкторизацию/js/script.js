6536b33ef2cb3641d4d0795e8ba3571423c68bcc
Bogdan Larionov <larionovbogdan1990@gmail.com>
2023-04-11 11:31:13 +0200

    add second method

--- L-29/Второй_Метод_через_Диструкторизацию/js/script.js
+++ L-29/Второй_Метод_через_Диструкторизацию/js/script.js
@@ -7,9 +7,10 @@ const workersForm = document.querySelector(".workers_form");
 const cardContainer = document.querySelector(".cards_container");
 let cards_info = [];
 
+// 2. из массива cards_info достаем данные и рендерим из них карточки
 const render = () => {
   cardContainer.innerText = ""; // что-бы не дублировались карточки!// очищаем контейнер перед новым рендером
-  cards_info.forEach((elem) => {
+  cards_info.forEach(({ firstname, lastname, email, link, progress }) => {
     const card = document.createElement("div");
     const nameElem = document.createElement("p");
     const lastnameElem = document.createElement("p");
@@ -21,22 +22,22 @@ const render = () => {
     const progressLine = document.createElement("div");
     const progressValue = document.createElement("p");
 
-    nameElem.innerText = `Name: ${elem.firstname} ${elem.lastname}`;
+    nameElem.innerText = `Name: ${firstname} ${lastname}`;
     mailElem.innerText = `Email: `;
-    mailText.innerText = `${elem.email}`;
+    mailText.innerText = `${email}`;
 
-    mailText.href = `mailto:${elem.email}`;
+    mailText.href = `mailto:${email}`;
     mailText.style.textDecoration = "none";
     mailText.style.color = "black";
-    photoLinkElem.src = `${elem.link}`;
-    progressValue.innerText = `${elem.progress} %`;
+    photoLinkElem.src = `${link}`;
+    progressValue.innerText = `${progress} %`;
     photoLinkElem.alt = `photo`;
     photoLinkElem.classList.add("img");
 
-    progressLine.style.width = elem.progress + "%";
+    progressLine.style.width = progress + "%";
 
     progressLine.style.backgroundColor =
-      elem.progress >= 75 ? "green" : elem.progress >= 50 ? "yellow" : "red";
+      progress >= 75 ? "green" : progress >= 50 ? "yellow" : "red";
 
     card.classList.add("card");
     progressContainer.classList.add("progress-container");
@@ -57,6 +58,7 @@ const render = () => {
   });
 };
 
+//1. при отправке формы данные записываются в массив cards_info в виде объектов + вызывается функция рендера
 workersForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
