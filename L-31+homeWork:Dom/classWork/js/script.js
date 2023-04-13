4de991789a6d310325d2e34c20c121457c46b4a7
Bogdan Larionov <larionovbogdan1990@gmail.com>
2023-04-13 21:19:04 +0200

    slider added

--- L-31+homeWork:Dom/classWork/js/script.js
+++ L-31+homeWork:Dom/classWork/js/script.js
@@ -78,3 +78,65 @@ changeModeBtn_two.addEventListener("click", () => {
   document.body.style.backgroundColor = background_Color;
 });
 // ___________________________________________
+// Менять цвет кнопки на случайный
+
+const random_color_btn = document.querySelector(".random_calor_btn");
+const getRandomNum = () => Math.round(Math.random() * 255);
+
+//Math.random() => дробное от 0 до 1
+//Math.random() * 255 => дробное от 0 до 255
+//Math.round(Math.random() * 255) => целое число от 0 до 255
+
+random_color_btn.addEventListener(
+  "click",
+  (event) =>
+    (event.target.style.backgroundColor = `rgb(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()})`)
+);
+// ___________________________________________
+
+//Менять цвет фон на случайный
+const background = document.querySelector(".random_bacground_calor_btn");
+const getRandom = () => Math.round(Math.random() * 255);
+
+background.addEventListener("click", () => {
+  document.body.style.backgroundColor = `rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`;
+});
+// ___________________________________________
+//Менять содержимое инпута на значение из массива
+
+const changeBtn = document.querySelector(".change_btn");
+const changeInput = document.querySelector(".change");
+
+const symbols = ["...", "!!!", "+++", "???"];
+let symvals_index = 0;
+
+changeBtn.addEventListener("click", () => {
+  changeInput.value = symbols[symvals_index++ % symbols.length];
+});
+
+//symbols_index++ % symbols.length
+// 0 % 4 = 0
+// 1 % 4 = 1
+// 2 % 4 = 2
+// 3 % 4 = 3
+// 4 % 4 = 0
+// 5 % 4 = 1
+// 6 % 4 = 2
+// 7 % 4 = 3
+// 8 % 4 = 0
+// ___________________________________________
+//Создать слайдер с фотографиями из массива
+
+const imgSlider = document.querySelector(".img_slider");
+const photos = [
+  "https://cdnn21.img.ria.ru/images/153009/29/1530092975_240:0:1680:1440_1920x0_80_0_0_9df118e773d0e9f5c788eabb7b6f2aad.jpg",
+  "https://cdnn21.img.ria.ru/images/153009/29/1530092975_240:0:1680:1440_1920x0_80_0_0_9df118e773d0e9f5c788eabb7b6f2aad.jpg",
+  "https://i.ytimg.com/vi/ViAath1B5WY/maxresdefault.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/African_Elephant_%28Loxodonta_africana%29_male_%2817289351322%29.jpg/1200px-African_Elephant_%28Loxodonta_africana%29_male_%2817289351322%29.jpg",
+];
+
+let photoIndex = 0;
+
+imgSlider.addEventListener("click", (event) => {
+  event.target.src = photos[photoIndex++ % photos.length];
+});
