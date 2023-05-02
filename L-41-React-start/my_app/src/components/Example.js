53c25f3717485a77f246bf5213cfc9fe6ac1b373
Bogdan Larionov <larionovbogdan1990@gmail.com>
2023-05-02 22:37:26 +0200

    first react app

---
+++ L-41-React-start/my_app/src/components/Example.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Example({ first_name, last_name }) {
+  return (
+    <div>
+      <p>Firstname: {first_name}</p>
+      <p>Lastname: {last_name}</p>
+    </div>
+  );
+}
