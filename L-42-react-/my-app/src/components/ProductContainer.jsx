7816200b38f58c644aebd25542d4faf5b12a08f8
Bogdan Larionov <larionovbogdan1990@gmail.com>
2023-05-03 13:52:36 +0200

    react praktika Gayk

---
+++ L-42-react-/my-app/src/components/ProductContainer.jsx
@@ -0,0 +1,12 @@
+import Product from "./Product";
+
+export default function productContainer() {
+  return (
+    <div>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
