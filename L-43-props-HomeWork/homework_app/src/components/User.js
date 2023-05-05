182c13f1923a8673a0d75e0aec3e59d5822dca83
Bogdan Larionov <larionovbogdan1990@gmail.com>
2023-05-05 10:53:40 +0200

    homeWork done

---
+++ L-43-props-HomeWork/homework_app/src/components/User.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function User({ id, firstname, lastname, address, salary }) {
+  const user_style = {
+    border: "1px solid black",
+    borderRadius: "10px",
+    padding: "10px",
+    width: "200px",
+    margin: "10px",
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  return (
+    <div style={user_style}>
+      <p>ID:{id}</p>
+      <p>First name: {firstname}</p>
+      <p>Last name:{lastname}</p>
+      <p>Address: {address}</p>
+      <p>Salary: {salary}</p>
+    </div>
+  );
+}
